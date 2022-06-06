52ab3fd85a47b53a86b70904ff0dfbde00c73bf9
Kay Robbins <1189050+VisLab@users.noreply.github.com>
2022-06-06 12:26:43 -0500

    Updated the requirements and printed cache location

--- tests/test_web_base.py
+++ tests/test_web_base.py
@@ -17,6 +17,7 @@ class TestWebBase(unittest.TestCase):
         with app.app_context():
             import hed.schema as hedschema
             hedschema.set_cache_directory(app.config['HED_CACHE_FOLDER'])
+
             if cls.cache_schemas:
                 hedschema.cache_xml_versions()
             from hedweb.routes import route_blueprint
