a66a2aaf21e55ac84427fe16fba824326187d86b
Kay Robbins <1189050+VisLab@users.noreply.github.com>
2024-10-07 16:15:29 -0500

    Updated default schema in schema validation to 8.3.0

--- hedweb/templates/js/constants.js
+++ hedweb/templates/js/constants.js
@@ -1,4 +1,4 @@
-const DEFAULT_XML_URL = "https://raw.githubusercontent.com/hed-standard/hed-schemas/main/standard_schema/hedxml/HED8.2.0.xml";
+const DEFAULT_XML_URL = "https://raw.githubusercontent.com/hed-standard/hed-schemas/main/standard_schema/hedxml/HED8.3.0.xml";
 const EXCEL_FILE_EXTENSIONS = ['xlsx', 'xls'];
 const JSON_FILE_EXTENSIONS = ['json'];
 const OTHER_VERSION_OPTION = 'Other';
