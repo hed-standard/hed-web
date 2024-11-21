6f764274b93af22e8ba5043637f2ff347f76d369
Kay Robbins <1189050+VisLab@users.noreply.github.com>
2024-11-21 14:21:47 -0600

    Fixed the test errors due to change in format

--- tests/test_routes/test_routes_services_events.py
+++ tests/test_routes/test_routes_services_events.py
@@ -25,16 +25,16 @@ class Test(TestRouteBase):
     def test_submit_service_events_validate_route_file_invalid(self):
         with self.app.app_context():
             json_data = {bc.CHECK_FOR_WARNINGS: 'on',
-                         bc.SCHEMA_VERSION: '8.2.0',
+                         bc.SCHEMA_VERSION: '8.3.0',
                          bc.EVENTS_STRING: "bad file",
-                         bc.SIDECAR_STRING: self._get_file_string("bids_events_bad.json"),
+                         bc.SIDECAR_STRING: self._get_file_string("bids_events.json"),
                          bc.SERVICE: 'events_validate'}
 
             response = self.app.test.post('/services_submit', content_type='application/json',
                                           data=json.dumps(json_data))
             json_data2 = json.loads(response.data)
-            had_error = json_data2.get("error_type")
-            self.assertTrue(had_error)
+            results = json_data2.get["results"]
+            self.assertEqual('warning', results["msg_category"], 'should be warning when errors')
 
     def test_submit_service_events_validate_route_file_issues(self):
         with self.app.app_context():
@@ -48,5 +48,6 @@ class Test(TestRouteBase):
                                           data=json.dumps(json_data))
             json_data2 = json.loads(response.data)
             results = json_data2['results']
+            self.assertEqual(results['msg_category'], 'warning', 'it should have warning')
             self.assertTrue('data' in results, 'should have a data key when no errors')
-            self.assertEqual('warning', results["msg_category"], 'should be warning when errors')
+
